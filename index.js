a3b6a51394b7233bcb1a4756df058dd09c0a1b34
Ubuntu <ubuntu@ip-172-26-13-29.ec2.internal>
2019-10-26 01:56:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+
+const express = require("express");
+const vhost = require("vhost");
+const https = require("https");
+const fs = require("fs");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const subdomains = {};
+const domains = process.env.DOMAINS.split(":");
+
+var staticFolder = fs.readdirSync("./static");
+var routersFolder = fs.readdirSync("./routers");
+
+for (let folder of staticFolder) {
+    subdomains[folder] = express.static(`./static/${folder}/`);
+}
+
+for (let folder of routersFolder) {
+    subdomains[folder] = require(`./routers/${folder}`);
+}
+
+for (let domain of domains) {
+    for (let subdomain in subdomains) {
+        app.use(vhost(`${subdomain}.${domain}`, subdomains[subdomain]));
+    }
+    
+    if ("www" in subdomains) {
+        app.use(vhost(`${domain}`, subdomains.www));
+    }
+}
+
+https.createServer({
+    key: fs.readFileSync(process.env.SSL_KEY),
+    cert: fs.readFileSync(process.env.SSL_CERT)
+}, app).listen(process.env.PORT, () => console.log("Listening."));
